d6c192cba83e3146b8f44adf6e0f9070b9b7aadc
Leonid Proskuryakov <lpr@inbox.ru>
2025-12-10 12:59:32 +0300

    Из тестов удалена часть проверок ручной валидации в соответствие с изменениями в контроллерах.

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -30,64 +30,20 @@ class FilmControllerTest {
     }
 
     @Test
-    void testNameCannotBeEmpty() {
-        Film film = new Film();
-        film.setDescription("Desc");
-        film.setReleaseDate(LocalDate.of(2000, 1, 1));
-        film.setDuration(120);
-
-        film.setName(null);
-        assertThrows(ValidationException.class, () -> controller.create(film));
-
-        film.setName("");
-        assertThrows(ValidationException.class, () -> controller.create(film));
-    }
-
-    @Test
-    void testDescriptionLength() {
-        Film film = new Film();
-        film.setName("Film");
-        film.setReleaseDate(LocalDate.of(2000, 1, 1));
-        film.setDuration(100);
-
-        film.setDescription("x".repeat(201));
-        assertThrows(ValidationException.class, () -> controller.create(film));
-
-        film.setDescription("x".repeat(200));
-        assertDoesNotThrow(() -> controller.create(film));
-    }
-
-    @Test
     void testReleaseDateValidation() {
         Film film = new Film();
         film.setName("Film");
         film.setDescription("Desc");
         film.setDuration(100);
 
-        film.setReleaseDate(LocalDate.of(1895, 12, 27));
+        film.setReleaseDate(FilmController.BIRHDAY_OF_CINEMA.minusDays(1));
         assertThrows(ValidationException.class, () -> controller.create(film));
 
-        film.setReleaseDate(LocalDate.of(1895, 12, 28));
+        film.setReleaseDate(FilmController.BIRHDAY_OF_CINEMA);
         assertDoesNotThrow(() -> controller.create(film));
 
         film.setReleaseDate(LocalDate.now().plusDays(1));
         assertDoesNotThrow(() -> controller.create(film)); // по твоей текущей логике проверка будущей даты не реализована
     }
 
-    @Test
-    void testDurationValidation() {
-        Film film = new Film();
-        film.setName("Film");
-        film.setDescription("Desc");
-        film.setReleaseDate(LocalDate.of(2000, 1, 1));
-
-        film.setDuration(0);
-        assertThrows(ValidationException.class, () -> controller.create(film));
-
-        film.setDuration(-5);
-        assertThrows(ValidationException.class, () -> controller.create(film));
-
-        film.setDuration(10);
-        assertDoesNotThrow(() -> controller.create(film));
-    }
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
@@ -26,51 +25,6 @@ class UserControllerTest {
         user.setName("Name");
         user.setBirthday(LocalDate.of(2000, 1, 1));
 
-        assertDoesNotThrow(() -> controller.create(user));
-    }
-
-    @Test
-    void testEmailValidation() {
-        User user = new User();
-        user.setLogin("login");
-        user.setBirthday(LocalDate.of(2000, 1, 1));
-
-        user.setEmail(null);
-        assertThrows(ValidationException.class, () -> controller.create(user));
-
-        user.setEmail("");
-        assertThrows(ValidationException.class, () -> controller.create(user));
-
-        user.setEmail("without-a-dog");
-        assertThrows(ValidationException.class, () -> controller.create(user));
-    }
-
-    @Test
-    void testLoginValidation() {
-        User user = new User();
-        user.setEmail("test@example.com");
-        user.setBirthday(LocalDate.of(2000, 1, 1));
-
-        user.setLogin(null);
-        assertThrows(ValidationException.class, () -> controller.create(user));
-
-        user.setLogin("");
-        assertThrows(ValidationException.class, () -> controller.create(user));
-
-        user.setLogin("with space");
-        assertThrows(ValidationException.class, () -> controller.create(user));
-    }
-
-    @Test
-    void testBirthdayValidation() {
-        User user = new User();
-        user.setEmail("test@example.com");
-        user.setLogin("login");
-
-        user.setBirthday(LocalDate.now().plusDays(1));
-        assertThrows(ValidationException.class, () -> controller.create(user));
-
-        user.setBirthday(LocalDate.of(2000, 1, 1));
         assertDoesNotThrow(() -> controller.create(user));
     }
 
