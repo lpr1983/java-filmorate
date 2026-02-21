ebe775f79032d9ef99035e1b39ff313fb1b597f5
Leonid Proskuryakov <lpr@inbox.ru>
2026-02-21 21:06:48 +0300

    Поправил недоработку в reviews. Вместо default, параметр аннотации required = false. Документация по spring: "By default, method parameters that use this annotation are required, but you can specify that a method parameter is optional by setting the @RequestParam annotation’s required flag to false or by declaring the argument with a java.util.Optional wrapper."

--- src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -40,11 +40,12 @@ public class ReviewController {
     }
 
     @GetMapping
-    public List<Review> getReviews(@RequestParam(defaultValue = "0") int filmId,
+    public List<Review> getReviews(@RequestParam(required = false) Integer filmId,
                                    @RequestParam(defaultValue = "10") int count) {
-        if (filmId == 0) {
+        if (filmId == null) {
             return reviewService.getAllReviews(count);
         }
+
         return reviewService.getReviewsByFilmId(filmId, count);
     }
 
