0f3ddf5b2be162b2f8c8d362a4fa3799222ba33a
Leonid Proskuryakov <lpr@inbox.ru>
2026-02-24 15:41:46 +0300

    По совету коллеги убрал комментарии и разделил метод на 2.

--- src/main/java/ru/yandex/practicum/filmorate/storage/recommendations/RecommendationsDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/recommendations/RecommendationsDbStorage.java
@@ -18,27 +18,52 @@ import java.util.List;
 public class RecommendationsDbStorage {
     private final NamedParameterJdbcTemplate jdbc;
     private final RowMapper<Film> mapper;
+    private static final String BASE_SELECT_FILMS_QUERY = """
+               SELECT f.id,
+                      f.name,
+                      f.description,
+                      f.release_date,
+                      f.duration,
+                      f.mpa_rating_id AS mpa_rating_id,
+                      m.name          AS mpa_name,
+                      m.age           AS mpa_age
+                FROM films f
+                LEFT JOIN mpa_ratings m ON m.id = f.mpa_rating_id
+            """;
 
     public List<Film> getRecommendations(int userId) {
         log.info("getRecommendations userId={}", userId);
 
-//        Из ТЗ:
-//        1) Найти пользователей с максимальным количеством пересечения по лайкам.
-//        2) Определить фильмы, которые один пролайкал, а другой нет.
-//        3) Рекомендовать фильмы, которым поставил лайк пользователь с похожими вкусами,
-//        а тот, для кого составляется рекомендация, ещё не поставил.
+        // Id фильмов, пролайканных максимально похожими по лайкам пользователями, которые не смотрел userId.
+        String recommendedQuery = """
+                SELECT l.film_id AS film_id,
+                       COUNT(*)  AS like_count
+                FROM likes l
+                LEFT JOIN likes my
+                ON my.user_id = :userId AND my.film_id = l.film_id
+                WHERE l.user_id IN (:mostSimilarUsersIds) AND my.film_id IS NULL
+                GROUP BY l.film_id
+                """;
 
-        // Делается 2мя запросами с промежуточной обработкой, т.к.
-        // А) Один запрос с подзапросами очень плохо читается.
-        // Б) Логика определения пользователей с максимальным количеством лайков может измениться.
-        // В) Не с WITH т.к. в режиме H2 с текущей конфигурацией работало неожиданно по сравнению с подзапросом.
-        // Запрос возвращало 0 строк при наличии пересечений. При этом в dbBeaver запрос отрабатывал корректно.
+        String queryStep2and3 = BASE_SELECT_FILMS_QUERY + """
+                JOIN (
+                """ + recommendedQuery + """
+                ) recommended ON recommended.film_id = f.id
+                ORDER BY recommended.like_count DESC, f.id
+                """;
 
-        // Шаг 1. Найти пользователей с максимальным количеством пересечения по лайкам.
+        List<Integer> mostSimilarUsersIds = getMostSimilarUsersIds(userId);
+
         MapSqlParameterSource params = new MapSqlParameterSource()
-                .addValue("userId", userId);
+                .addValue("userId", userId)
+                .addValue("mostSimilarUsersIds", mostSimilarUsersIds);
 
-        String queryStep1 = """
+        return jdbc.query(queryStep2and3, params, mapper);
+    }
+
+    private List<Integer> getMostSimilarUsersIds(int userId) {
+
+        String query = """
                 SELECT l2.user_id AS user_id,
                        COUNT(*)   AS common_count
                 FROM likes l1
@@ -49,7 +74,10 @@ public class RecommendationsDbStorage {
                 ORDER BY common_count DESC, l2.user_id;
                 """;
 
-        List<Integer> maxCommonUsersIds = jdbc.query(queryStep1, params, rs -> {
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("userId", userId);
+
+        List<Integer> mostSimilarUsersIds = jdbc.query(query, params, rs -> {
             List<Integer> result = new ArrayList<>();
 
             Integer maxCommonCount = null;
@@ -66,49 +94,11 @@ public class RecommendationsDbStorage {
             return result;
         });
 
-        // Если нет пользователей с пересечениями — рекомендаций нет
-        if (maxCommonUsersIds == null || maxCommonUsersIds.isEmpty()) {
+        if (mostSimilarUsersIds == null || mostSimilarUsersIds.isEmpty()) {
             return Collections.emptyList();
         }
 
-        params.addValue("maxCommonUsersIds", maxCommonUsersIds);
-
-        // Шаг 2,3. Определить фильмы, которые один пролайкал, а другой нет.
-        // Рекомендовать фильмы, которым поставил лайк пользователь с похожими вкусами,
-        // а тот, для кого составляется рекомендация, ещё не поставил.
-        String baseFilmQuery = """
-                   SELECT f.id,
-                          f.name,
-                          f.description,
-                          f.release_date,
-                          f.duration,
-                          f.mpa_rating_id AS mpa_rating_id,
-                          m.name          AS mpa_name,
-                          m.age           AS mpa_age
-                    FROM films f
-                    LEFT JOIN mpa_ratings m ON m.id = f.mpa_rating_id
-                """;
-
-        String recommendedQuery = """
-        SELECT
-            l.film_id,
-            COUNT(*) AS like_count
-        FROM likes l
-        LEFT JOIN likes my
-               ON my.user_id = :userId
-              AND my.film_id = l.film_id
-        WHERE l.user_id IN (:maxCommonUsersIds)
-          AND my.film_id IS NULL
-        GROUP BY l.film_id
-        """;
-
-        String queryStep2and3 = baseFilmQuery + """
-        JOIN (
-        """ + recommendedQuery + """
-        ) recommended ON recommended.film_id = f.id
-        ORDER BY recommended.like_count DESC, f.id
-        """;
-
-        return jdbc.query(queryStep2and3, params, mapper);
+        return mostSimilarUsersIds;
     }
+
 }
